43e8008aa37bffe3f1230b2c6fc5f9cd31274303
RoyLin <1002591652@qq.com>
2024-11-29 18:31:20 +0800

    feat: dynamic title

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import type { DocsThemeConfig } from 'nextra-theme-docs'
+import { useConfig, DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <b>SpringBoot VS NestJS</b>,
-  head: (
-    <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="SVN" />
-      <meta property="og:description" content="SpringBoot VS NestJS" />
-    </>
-  ),
+  head() {
+    const { title } = useConfig();
+    return (
+      <>
+        <title>{title} - SVN</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="SVN" />
+        <meta property="og:description" content="SpringBoot VS NestJS" />
+      </>
+    );
+  },
   project: {
     link: 'https://github.com/ZhiXiao-Lin/svn'
   },

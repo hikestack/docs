61a6ceb1733e2d2d11af102c82ffc3dea28f357d
RoyLin <1002591652@qq.com>
2024-11-29 18:24:31 +0800

    docs: page title

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,12 +2,19 @@ import React from 'react'
 import type { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <b>SVN</b>,
+  logo: <b>SpringBoot VS NestJS</b>,
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="SVN" />
+      <meta property="og:description" content="SpringBoot VS NestJS" />
+    </>
+  ),
   project: {
     link: 'https://github.com/ZhiXiao-Lin/svn'
   },
   footer: {
-    content: `Ell JS @ ${new Date().getFullYear()}`,
+    content: `EllJS @ ${new Date().getFullYear()}`,
   },
 }
 

f153a78645e5ad38a514fba9711011dc9a0a6403
RoyLin <18770221825@163.com>
2024-12-02 00:11:06 +0800

    feat: 新增PythonModule

--- pages/modules/_meta.js
+++ pages/modules/_meta.js
@@ -1,44 +1,47 @@
 export default {
-  "logger": {
-    title: "日志模块"
-  },
-  "mail": {
-    title: "邮件模块"
-  },
-  "sms": {
-    title: "短信模块"
-  },
-  "hash": {
-    title: "哈希模块"
-  },
-  "file": {
-    title: "文件模块"
-  },
-  "oauth": {
-    title: "OAuth模块"
-  },
-  "redis": {
-    title: "Redis模块"
-  },
-  "lock": {
-    title: "分布式锁模块"
-  },
-  "saga": {
-    title: "分布式事务模块"
-  },
-  "prisma": {
-    title: "Prisma模块"
-  },
-  "kysely": {
-    title: "Kysely模块"
-  },
-  "drizzle": {
-    title: "Drizzle模块"
-  },
-  "jvm": {
-    title: "JVM模块"
-  },
-  "service": {
-    title: "云服务"
-  }
-}
+	logger: {
+		title: "日志模块",
+	},
+	mail: {
+		title: "邮件模块",
+	},
+	sms: {
+		title: "短信模块",
+	},
+	hash: {
+		title: "哈希模块",
+	},
+	file: {
+		title: "文件模块",
+	},
+	oauth: {
+		title: "OAuth模块",
+	},
+	redis: {
+		title: "Redis模块",
+	},
+	lock: {
+		title: "分布式锁模块",
+	},
+	saga: {
+		title: "分布式事务模块",
+	},
+	prisma: {
+		title: "Prisma模块",
+	},
+	kysely: {
+		title: "Kysely模块",
+	},
+	drizzle: {
+		title: "Drizzle模块",
+	},
+	jvm: {
+		title: "JVM模块",
+	},
+	python: {
+		title: "Python模块",
+	},
+	service: {
+		title: "云服务",
+	},
+};

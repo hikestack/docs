c1f9261293c885ac94555ee271b7b9d0e5148d95
RoyLin <1002591652@qq.com>
2024-12-01 15:15:13 +0800

    feat: add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
+import { GoogleTagManager } from '@next/third-parties/google'
 import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GoogleTagManager gtmId="G-G1G5G0PTKD" />
+      <Component {...pageProps} />
+    </>
+  );
 }
